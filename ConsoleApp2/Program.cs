2182e0f3696c59202b8ff594e2d25b048e11e7a1
liquidsamlol <liquidsamlol@gmail.com>
2018-09-27 05:48:05 +0300

    done

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double p = 3.14;
+            double r;
+            double s;
+            double square;
+            Console.WriteLine("Введите радиус круга:");
+            r = Console.Read();
+            square = Math.Pow(r, 2);
+            s = p * square;
+            Console.WriteLine("Площадь круга равна" + s);
+            
+
+        }
+    }
+}
